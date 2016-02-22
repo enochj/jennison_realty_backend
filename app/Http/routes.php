99a1545a3839e10e00d7b57c9df43cd797802856
enochj <enochjennison@gmail.com>
2016-02-22 11:22:39 -0600

    Cleaned up Routes.php
    There was some repetitive code that I extracted to an outside function.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,24 +4,31 @@ use App\Photo;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 
+function mergeWithPhotos ($array) {
+  foreach ($listings as $listing) {
+    $id = $listing["id"];
+    $photos = Photo::select('photos.mediamodificationtimestamp', 'photos.mediaurl')->where('listing_id','=',$id)->get()->toArray();
+    $photo_array[] = $photos;
+    $listing["Photos"] = $photos;
+    $merged_listings[] = $listing;
+	}
+  return $merged_listings;
+}
+
 //This is the basic get function localhost/laravelapp/realty/public/
 Route::get('/', function () {
 	$listings = Listing::get()->toArray();
 	$merged_listings;
-	foreach ($listings as $listing) {
-		$id = $listing["id"];
-		$photos = Photo::select('photos.mediamodificationtimestamp', 'photos.mediaurl')->where('listing_id','=',$id)->get()->toArray();
-		$photo_array[] = $photos;
-		$listing["Photos"] = $photos;
-		$merged_listings[] = $listing;
-	}	
+//We must merge the listing results with the photo results.
+  $merged_listings = mergeWithPhotos($listings);
 	return json_encode($merged_listings);
 });
 
-//The formate of this get is localhost/laravelapp/realty/public/page?filter=ListPrice&sort=asc
+//The format of this get is localhost/laravelapp/realty/public/page?filter=<ListPrice>&sort=<asc>
 //Options for filter are ListPrice, ListingDate, and Photos.
 //Options for sort are asc and des.
 Route::get('/page/', function () {
+//Get filters.
 	if (isset($_GET["page"]))
 		$pager = ($_GET["page"]);
 	else $pager = 15;
@@ -37,24 +44,12 @@ Route::get('/page/', function () {
 //Here we have the case of ListPrice and ascending.
 				case "asc":
 					$array = Listing::all()->sortBy('listprice');
-					foreach ($array as $listing) {
-						$id = ($listing["id"]);
-						$photos = Photo::select('photos.mediamodificationtimestamp', 'photos.mediaurl')->where('listing_id','=',$id)->get()->toArray();
-						$photo_array[] = $photos;
-						$listing["Photos"] = $photos;
-						$merged_listings[] = $listing;
-					}
+          $merged_listings = mergeWithPhotos($array);
 					break;
 //Here we have the case of ListPrice and descending.
 				case "des":
 					$array = Listing::all()->sortByDesc('listprice');
-					foreach ($array as $listing) {
-						$id = ($listing["id"]);
-						$photos = Photo::select('photos.mediamodificationtimestamp', 'photos.mediaurl')->where('listing_id','=',$id)->get()->toArray();
-						$photo_array[] = $photos;
-						$listing["Photos"] = $photos;
-						$merged_listings[] = $listing;
-					}					
+          $merged_listings = mergeWithPhotos($array);					
 					break;
 			}
 			break;
@@ -63,24 +58,12 @@ Route::get('/page/', function () {
 			switch ($sort) {
 				case "asc":
 					$array = Listing::all()->sortBy('created_at');
-					foreach ($array as $listing) {
-						$id = ($listing["id"]);
-						$photos = Photo::select('photos.mediamodificationtimestamp', 'photos.mediaurl')->where('listing_id','=',$id)->get()->toArray();
-						$photo_array[] = $photos;
-						$listing["Photos"] = $photos;
-						$merged_listings[] = $listing;
-					}
+          $merged_listings = mergeWithPhotos($array);
 					break;
 //Here we have the case of ListingDate and descending.
 				case "des":
 					$array = Listing::all()->sortByDesc('created_at');
-					foreach ($array as $listing) {
-						$id = ($listing["id"]);
-						$photos = Photo::select('photos.mediamodificationtimestamp', 'photos.mediaurl')->where('listing_id','=',$id)->get()->toArray();
-						$photo_array[] = $photos;
-						$listing["Photos"] = $photos;
-						$merged_listings[] = $listing;
-					}
+					$merged_listings = mergeWithPhotos($array);
 					break;
 			}
 			break;
@@ -90,8 +73,8 @@ Route::get('/page/', function () {
 	return new Paginator($array, $pager);
 });
 
+//Toggle the listingstatus. Format will look like localhost/laravelapp/realty/public/status?id=<14>
 Route::get('/status', function () {
-//Toggle the listingstatus.
 	if (isset($_GET["id"])) {
 		$id = ($_GET["id"]);
 		$listing = Listing::find($id);
@@ -101,17 +84,10 @@ Route::get('/status', function () {
 			$listing->listingstatus = "Active";
 		$listing->save();
 	}
-
 //Return all results joined with photos.
 	$listings = Listing::get()->toArray();
 	$merged_listings;
-	foreach ($listings as $listing) {
-		$id = $listing["id"];
-		$photos = Photo::select('photos.mediamodificationtimestamp', 'photos.mediaurl')->where('listing_id','=',$id)->get()->toArray();
-		$photo_array[] = $photos;
-		$listing["Photos"] = $photos;
-		$merged_listings[] = $listing;
-	}	
+  $merged_listings = mergeWithPhotos($listings);
 	return json_encode($merged_listings);
 });
 
